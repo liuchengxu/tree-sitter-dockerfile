0f648be5fd233a9be3428ec12de66a8be2dce0b6
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2024-09-21 16:12:43 +0800

    Update tree-sitter to 0.23

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_dockerfile::language()).expect("Error loading dockerfile grammar");
+//! parser.set_language(&tree_sitter_dockerfile::LANGUAGE.into()).expect("Error loading dockerfile grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -15,18 +15,14 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_dockerfile() -> Language;
+    fn tree_sitter_dockerfile() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
-///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_dockerfile() }
-}
+/// The tree-sitter [`LanguageFn`] for this grammar.
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_dockerfile) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -46,7 +42,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::LANGUAGE.into())
             .expect("Error loading dockerfile language");
     }
 }
